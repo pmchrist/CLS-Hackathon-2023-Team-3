6c1f603486e36817c2d3ca27781acea68e408144
pmchrist <pmchrist@protonmail.com>
2023-04-22 13:17:20 +0200

    Added River's Code

--- everything_else.py
+++ everything_else.py
@@ -1,92 +1,114 @@
-import mesa
 from mesa import Model, Agent
 from mesa.time import RandomActivation
 from mesa.space import NetworkGrid
 import networkx as nx
+import numpy as np
 import random
 
-# Agent
-class Person:
-    def __init__(self, unique_id, age, drinking_behavior, social_norms, social_motives):
-        print(unique_id)
+class SocialNorms(Model):
+    def _init_(self, num_agents):
+        self.num_agents = num_agents
+        self.graph_cluster_size = 0.01
+        self.graph_interconnectivity = 0.01
+
+        self.schedule = RandomActivation(self)
+        self.grid = NetworkGrid(nx.Graph())
+        self.G = self.create_social_network()
+        self.create_agents()
+        self.running = True
+
+    def create_social_network(self):
+        network = nx.Graph()
+        #network = nx.newman_watts_strogatz_graph(n = self.num_agents, k = int(self.num_agents*self.graph_cluster_size), p = int(self.num_agents*self.graph_interconnectivity))
+        network.add_nodes_from(range(self.num_agents))
+        for i in range(self.num_agents):
+            for j in range(i+1, self.num_agents):
+                if random.random() < 0.3:
+                    network.add_edge(i, j)
+        return network
+
+    def create_agents(self):
+        for i in range(self.num_agents):
+            age = random.randint(18, 65)
+            alcohol = random.choice([True, False])
+            if age < 25:
+                average_drinks = np.random.lognormal(16, 1)
+                drinking_behavior = min(np.random.normal(0, 1) + np.random.normal(0, 0.15), 1)
+            else:
+                average_drinks = np.random.lognormal(6, 1)
+                drinking_behavior = np.random.normal(0, 1)
+
+            agent = Person(i, age, alcohol, average_drinks, drinking_behavior)
+            self.schedule.add(agent)
+            self.grid.place_agent(agent, i)
+    
+    def step(self):
+        self.schedule.step()
+
+class Person(Agent):
+    def _init_(self, unique_id, age, alcohol, average_drinks, drinking_behavior):
         self.unique_id = unique_id
         self.age = age
+        self.alcohol = alcohol
+
+        self.average_drinks = average_drinks
         self.drinking_behavior = drinking_behavior
-        self.social_norms = social_norms
-        self.social_motives = social_motives
-    
-    def step(self):
-        return
-
-# Model
-class SocialNorms(Model):
-    def __init__(self, num_agents):
-        self.num_agents = num_agents
-        self.schedule = RandomActivation(self)
-        graph_cluster_size = 0.01
-        graph_interconnectivity = 0.01
-        self.G = nx.newman_watts_strogatz_graph(n = num_agents, k = int(num_agents*graph_cluster_size), p = int(num_agents*graph_interconnectivity))
-        self.grid = NetworkGrid(self.G)
-        self.create_agents()
-
-        self.datacollector = mesa.DataCollector(
-            #model_reporters={"Gini": compute_gini},
-            #agent_reporters={"Wealth": lambda _: _.wealth},
-        )
-
-        self.list_of_random_nodes = self.random.sample(list(self.G), self.num_agents)
-
-        self.running = True
-        self.datacollector.collect(self)
-    
-
-    def step(self):
-        return
-    
-    def run_model(self, n):
-        for i in range(n):
-            self.step()
-    
-    def create_agents(self):
-        id = -1
-        for i in range(self.num_agents):
-            id += 1
-            age = random.randint(18, 65)            # Change age if we are doing Students
-            drinking_behavior = random.random()     # They do nothing for now
-            social_norms = random.random()
-            social_motives = random.random()
-            agent = Person(id, age, drinking_behavior, social_norms, social_motives)
-            self.schedule.add(agent)
-            self.grid.place_agent(agent, self.list_of_random_nodes[i])
-
-# Some function
-def get_opinion(self):
-    """Calculate the agent's opinion on alcohol consumption based on their own 
-    behavior and the behavior of their peers, taking into account their 
-    susceptibility to social influence.
-    """
-    peer_alcohol = [n.get_alcohol() for n in self.get_peers()]
-    num_peers = len(peer_alcohol)
-    
-    if self.alcohol:
-        # Agent drinks alcohol
-        if num_peers == 0:
-            # No peers to compare to
-            return 1
+        
+    def get_peers(self):
+        """Return a list of the agent's peers."""
+        peers = self.model.grid.get_neighbors(self.pos, include_center=False)
+        return peers
+        
+    def get_alcohol(self):
+        """Return the agent's alcohol consumption."""
+        if self.alcohol:
+            return self.average_drinks
         else:
-            # Compare to peers and adjust based on susceptibility to social influence
-            peer_avg = sum(peer_alcohol) / num_peers
-            return self.alcohol_influence * (self.alcohol / peer_avg) + (1 - self.alcohol_influence)
-    else:
-        # Agent does not drink alcohol
-        if num_peers == 0:
-            # No peers to compare to
-            return 0
-        else:
-            # Compare to peers and adjust based on susceptibility to social influence
-            peer_avg = sum(peer_alcohol) / num_peers
-            return self.alcohol_influence * (1 - self.alcohol / peer_avg) + (1 - self.alcohol_influence)
+            return
+        
+    def update_average_drinking(self):
+        """Update the agent's average number of drinks per week based on their social network."""
+        peer_alcohol = [n.get_alcohol() for n in self.get_peers()]
+        num_peers = len(peer_alcohol)
+        old_average_drinks = self.average_drinks
 
-def run_it(size, steps):
-    model = SocialNorms(size)
-    model.run_model(steps)
+        if self.alcohol:
+            # Agent drinks alcohol
+            if num_peers == 0:
+                # No peers to compare to
+                return 0
+            else:
+                # Compare to peers and adjust based on susceptibility to social influence
+                peer_avg = sum(peer_alcohol) / num_peers
+                # self.average_drinks = self.drinking_behavior * (self.average_drinks / peer_avg) + (1 - self.drinking_behavior)
+                self.average_drinks = self.drinking_behavior * (self.average_drinks / peer_avg) + np.normal(0, 0.5)
+
+                # return 1
+        else:
+            # Agent does not drink alcohol
+            if num_peers == 0:
+                # No peers to compare to
+                return 0
+            else:
+                # Compare to peers and adjust based on susceptibility to social influence
+                peer_avg = sum(peer_alcohol) / num_peers
+                # self.average_drinks = self.drinking_behavior * (1 - self.average_drinks / peer_avg) + (1 - self.drinking_behavior)
+                self.average_drinks = self.drinking_behavior * (1 - self.average_drinks / peer_avg) + np.normal(0, 0.5)
+                
+                # return 0
+    
+        self.update_drinking_behavior(old_average_drinks)
+        return 1
+
+    def update_drinking_behavior(self, old_average_drinks):
+        """Update the agent's drinking behavior based on their average number of drinks per week."""
+        if self.average_drinks > old_average_drinks:
+            self.drinking_behavior = min(self.drinking_behavior + np.normal(0, 0.05), 1)
+        else:
+            self.drinking_behavior = max(self.drinking_behavior - np.normal(0, 0.05), 0)
+
+    def step(self):
+        self.update_average_drinking()     
+        print(self.average_drinks)
+       
+
--- server.py
+++ server.py
@@ -1,49 +1,6 @@
 from everything_else import SocialNorms
 import mesa
 
-def network_portrayal(G):
-    # The model ensures there is 0 or 1 agent per node
-
-    portrayal = dict()
-    portrayal["nodes"] = [
-        {
-            "id": node_id,
-            "size": 3 if agents else 1,
-            "color": "#CC0000" if not agents or agents[0].age > 30 else "#007959",
-            "label": None
-            if not agents
-            else f"Agent:{agents[0].unique_id} Age:{agents[0].age}",
-        }
-        for (node_id, agents) in G.nodes.data("agent")
-    ]
-
-    portrayal["edges"] = [
-        {"id": edge_id, "source": source, "target": target, "color": "#000000"}
-        for edge_id, (source, target) in enumerate(G.edges)
-    ]
-
-    return portrayal
-
-
-grid = mesa.visualization.NetworkModule(network_portrayal, 500, 500)
-chart = mesa.visualization.ChartModule(
-    [], data_collector_name="datacollector"
-)
-
-model_params = {
-    "num_agents": mesa.visualization.Slider(
-        "Number of agents",
-        7,
-        2,
-        10,
-        1,
-        description="Choose how many agents to include in the model",
-    ),
-}
-
-server = mesa.visualization.ModularServer(
-    SocialNorms, [grid, chart], "Drinking Model", model_params
-)
-server.port = 8521
-
-server.launch()
+model = SocialNorms(10)
+for i in range(10):
+    model.step()
